0468ed38a56b42c3948047a1b266f3a81eb5037f
Arian / Reza <118453783+ArianReza@users.noreply.github.com>
2022-11-27 09:02:17 +0700

    Add files via upload

---
+++ galeri/main.js
@@ -0,0 +1,22 @@
+/* ==== MENU PHONE === */
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
